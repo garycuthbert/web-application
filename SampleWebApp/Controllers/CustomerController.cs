4ecfd288aa8e3ebc393046dd4e88b1974eeeea6b
garycuthbert <gary.cuthbert@calnexsol.com>
2014-07-26 00:15:10 +0100

    initial put of project files
    , hopefully not including transient build files

---
+++ SampleWebApp/Controllers/CustomerController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using SampleWebApp.Models;
+
+namespace SampleWebApp.Controllers
+{
+    public class CustomerController : ApiController
+    {
+        Customer[] customers = new Customer[]
+        {
+            new Customer { Id = 1, Name ="Company 1", SectorId = 1 },
+            new Customer { Id = 2, Name ="Company 2", SectorId = 121 },
+            new Customer { Id = 3, Name ="Company 3", SectorId = 1 },
+            new Customer { Id = 4, Name ="Company 4", SectorId = 142 }
+        };
+
+        // GET api/customer
+        public IEnumerable<Customer> GetAllCustomers()
+        {
+            return customers;
+        }
+
+        // GET api/customer/5
+        public IHttpActionResult GetCustomer(int id)
+        {
+            var customer = customers.FirstOrDefault((p) => p.Id == id);
+            if (null == customer)
+            {
+                return NotFound();
+            }
+
+            return Ok(customer);
+        }
+
+        // POST api/customer
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/customer/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/customer/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
