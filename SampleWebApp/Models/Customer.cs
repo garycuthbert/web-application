fee91cab4a67cf434421c498e115ba4119f3b7f6
garycuthbert <garycuthbert@users.noreply.github.com>
2014-07-26 02:14:36 +0100

    small change to test checkin

--- SampleWebApp/Models/Customer.cs
+++ SampleWebApp/Models/Customer.cs
@@ -10,5 +10,6 @@ namespace SampleWebApp.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public int SectorId { get; set; }
+        public string Address { get; set; }
     }
 }
